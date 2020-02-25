eb6d1f3aba2049cbddfe7e9d34eae8ed7e69dec8
Ozaka <ozaka@mail.ru>
2020-02-25 17:06:40 +0300

    Урок №2 Добавьте пустые классы для корзины товаров и элемента корзины товаров. Продумайте, какие методы понадобятся для работы с этими сущностями.

--- script.js
+++ script.js
@@ -1,52 +1,103 @@
-const goods = [
-    {
-        title: 'Super Mario Odyssey',
-        price: 150
-    },
-    {
-        title: 'Luigi Mansion 3',
-        price: 50
-    },
-    {
-        title: 'Astral Chain',
-        price: 350
-    },
-    {
-        title: 'The Legend of Zelda: Breath of the Wild',
-        price: 250
-    },
-    {
-        title: 'The Legend of Zelda: Link’s Awakening',
-        price: 70
-    },
-    {
-        title: 'Yoshi Crafted World',
-        price: 160
-    },
-    {
-        title: 'Stardew Valley',
-        price: 40
-    },
-    {
-        title: 'Captain Toad: Treasure Tracker',
-        price: 120
-    },
-    {
-        title: 'Super Mario Party',
-        price: 100
-    },
-    {
-        title: 'Rayman Legends: Definitive Edition (Nintendo Switch)',
-        price: 200
-    },
-];
+class ProductsList {
+    constructor(container = '.goods-list') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+    }
 
-const renderGoodsItem = (title, price, img = 'default_picture.jpg') => `<div class="goods-item"><img src="${img}" alt="${title}"><h3>${title}</h3><p class="price">${price} руб.</p><div class="btn_basket">В корзину</div></div>`;
+    _fetchProducts() {
+        this.goods = [
+            {
+                id: 1,
+                title: 'Super Mario Odyssey',
+                price: 150
+    },
+            {
+                id: 2,
+                title: 'Luigi Mansion 3',
+                price: 50
+    },
+            {
+                id: 3,
+                title: 'Astral Chain',
+                price: 350
+    },
+            {
+                id: 4,
+                title: 'The Legend of Zelda: Breath of the Wild',
+                price: 250
+    },
+            {
+                id: 5,
+                title: 'The Legend of Zelda: Link’s Awakening',
+                price: 70
+    },
+            {
+                id: 6,
+                title: 'Yoshi Crafted World',
+                price: 160
+    },
+            {
+                id: 7,
+                title: 'Stardew Valley',
+                price: 40
+    },
+            {
+                id: 8,
+                title: 'Captain Toad: Treasure Tracker',
+                price: 120
+    },
+            {
+                id: 9,
+                title: 'Super Mario Party',
+                price: 100
+    },
+            {
+                id: 10,
+                title: 'Rayman Legends: Definitive Edition (Nintendo Switch)',
+                price: 200
+    },
+        ];
+    }
+    
+    render() {
+        const block = document.querySelector(this.container);
+        for (let itemProduct of this.goods) {
+            const productObj = new ProductItem(itemProduct);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+        }
+    }
 
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join("");
-//    Вывод запятых происходит потому, что мы выводим содержимое массива. По умолчанию эллементы в массиве разделены запятыми.
 }
 
-renderGoodsList(goods);
+class ProductItem {
+    constructor(product, img = 'default_picture.jpg') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+
+    }
+
+    render() {
+        return `<div class="goods-item" data-id="${this.id}"><img src="${this.img}" alt="${this.title}"><h3>${this.title}</h3><p class="price">${this.price} руб.</p><div class="btn_basket">В корзину</div></div>`
+    }
+}
+
+class basket {
+    addBaket() {};
+    removeBasket() {};
+    clearBasket() {};
+}
+
+class basketItem {
+    plusBasketItem() {};
+    minusBasketItem() {};
+    deleteBasketItem() {};
+    
+}
+
+let list = new ProductsList();
+list.render();
